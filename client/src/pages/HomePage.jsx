6a3e4e2964832904b55841286538328a90e25f22
RiyaSingh-13 <dikshasinghrathore1213@gmail.com>
2026-03-12 23:36:14 +0530

    Update project files

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,19 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
 import Sidebar from '../components/Sidebar'
 
 const HomePage = () => {
+  const [selectUser, setSelectedUser] = useState(false) ; 
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
-      <div>
-         <Sidebar/>
-        <ChatContainer/>
-        <RightSidebar/>
-        
+      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative 
+         ${selectUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2 '}`}>
+          <Sidebar selectUser={selectUser} setSelectedUser={setSelectedUser}/>
+        <ChatContainer selectUser={selectUser} setSelectedUser={setSelectedUser}/>
+        <RightSidebar selectedUser={selectUser}/>
+
       </div>
     </div>
   )
