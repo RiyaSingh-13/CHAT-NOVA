20556ffc709e9ee47b5e706be7c046c443320800
RiyaSingh-13 <dikshasinghrathore1213@gmail.com>
2026-03-29 18:22:38 +0530

    fix: handle all OPTIONS requests in userRouter for CORS preflight

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,7 +6,13 @@ import {
   updateProfile,
 } from "../controllers/UserController.js";
 import { protectRoute } from "../middleware/auth.js";
+
 const userRouter = express.Router();
+
+// Handle all OPTIONS requests for preflight
+userRouter.options("*", (req, res) => {
+  res.sendStatus(204);
+});
 
 userRouter.post("/signup", signup);
 
